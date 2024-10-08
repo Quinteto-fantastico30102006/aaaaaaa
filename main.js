6d93b1a019704fa5994ca6e1593370ea935e3482
deixou a conta logada bbKKKKKKKKKKKKKKKKKKKK|kK|KKKKK| <128146736+Quinteto-fantastico30102006@users.noreply.github.com>
2024-10-08 09:22:20 -0300

    g

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Qual é a linguagem de programação mais popular em 2024?",
-        alternativas: ["JavaScript", "Python", "Java"],
+        alternativas: ["JavaScript", "Python"],
     },
     {
-    enunciado: "O que é a inteligência artificial?",
-    alternativas: ["Um tipo de hardware", "Um software que simula inteligência humana", "Um sistema operacional", "Uma linguagem de programação"],
+        enunciado: "O que é a inteligência artificial?",
+        alternativas: ["Um tipo de hardware", "Um software que simula inteligência humana"],
     },
     ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+    mostraPergunta();
