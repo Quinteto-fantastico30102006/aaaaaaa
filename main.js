a533c25332c1908495d3eeb075b4c4ed0e317033
deixou a conta logada bbKKKKKKKKKKKKKKKKKKKK|kK|KKKKK| <128146736+Quinteto-fantastico30102006@users.noreply.github.com>
2024-10-15 09:07:53 -0300

    .

--- main.js
+++ main.js
@@ -13,11 +13,54 @@ const perguntas = [
         enunciado: "O que é a inteligência artificial?",
         alternativas: ["Um tipo de hardware", "Um software que simula inteligência humana"],
     },
+    {
+        enunciado: "O que é o ChatGPT?",
+        alternativas: ["O ChatGPT é um modelo de linguagem desenvolvido pela OpenAI que utiliza inteligência artificial para gerar texto, responder perguntas e participar de conversas em linguagem natural."],
+    },
+    {
+        enunciado: "O ChatGPT tem sentimentos ou consciência?",
+        alternativas: ["Não, o ChatGPT não possui sentimentos, consciência ou emoções. Ele gera respostas baseadas em padrões de linguagem, sem compreensão real ou experiências pessoais."],
+    }
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        alternativas: ["Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro."],
+    }
     ];
-let atual = 0;
-let perguntaAtual;
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-}
+    let atual = 0;
+    let perguntaAtual;
+    let historiaFinal = "";
+    
+    function mostraPergunta() {
+        if (atual >= perguntas.length) {
+            mostraResultado();
+            return;
+        }
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        caixaAlternativas.textContent = "";
+        mostraAlternativas();
+    }
+    
+    function mostraAlternativas(){
+        for(const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+            caixaAlternativas.appendChild(botaoAlternativas);
+        }
+    }
+    
+    function respostaSelecionada(opcaoSelecionada) {
+        const afirmacoes = opcaoSelecionada.afirmacao;
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+    }
+    
+    function mostraResultado() {
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
+    }
+    
     mostraPergunta();
