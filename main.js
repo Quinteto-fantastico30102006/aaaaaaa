573f436afbea88cd9124b0bf819fee17e5a492fc
deixou a conta logada bbKKKKKKKKKKKKKKKKKKKK|kK|KKKKK| <128146736+Quinteto-fantastico30102006@users.noreply.github.com>
2024-10-15 09:19:37 -0300

    laura

--- main.js
+++ main.js
@@ -7,24 +7,31 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Qual é a linguagem de programação mais popular em 2024?",
-        alternativas: ["JavaScript", "Python"],
+        alternativas: [
+            "JavaScript", 
+            "Python"
+        ]
     },
     {
         enunciado: "O que é a inteligência artificial?",
-        alternativas: ["Um tipo de hardware", "Um software que simula inteligência humana"],
+        alternativas: ["Um tipo de hardware", "Um software que simula inteligência humana"
+        ]
     },
     {
         enunciado: "O que é o ChatGPT?",
-        alternativas: ["O ChatGPT é um modelo de linguagem desenvolvido pela OpenAI que utiliza inteligência artificial para gerar texto, responder perguntas e participar de conversas em linguagem natural."],
+        alternativas: ["O ChatGPT é um modelo de linguagem desenvolvido pela OpenAI que utiliza inteligência artificial para gerar texto, responder perguntas e participar de conversas em linguagem natural."
+        ]
     },
     {
         enunciado: "O ChatGPT tem sentimentos ou consciência?",
-        alternativas: ["Não, o ChatGPT não possui sentimentos, consciência ou emoções. Ele gera respostas baseadas em padrões de linguagem, sem compreensão real ou experiências pessoais."],
-    }
+        alternativas: ["Não, o ChatGPT não possui sentimentos, consciência ou emoções. Ele gera respostas baseadas em padrões de linguagem, sem compreensão real ou experiências pessoais.",
+        ]
+    },
     {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: ["Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro."],
-    }
+        alternativas: ["Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro."
+        ]
+    },
     ];
     let atual = 0;
     let perguntaAtual;
